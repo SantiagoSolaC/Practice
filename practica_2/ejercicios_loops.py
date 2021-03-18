a0d46c537e36072ee0c2e8d744563c7a7ada1f6a
SantiagoSolaC <santiagosolacosta@gmail.com>
2021-03-18 15:53:52 -0300

    practica_2

--- practica_2/ejercicios_loops.py
+++ practica_2/ejercicios_loops.py
@@ -5,11 +5,14 @@ vector: nombres = [‘Lucas’,’Franco’,’Luciano’,’Malena’,’Jonata
 Recorra el vector e imprima los elementos utilizando el ciclo for sin índices
 (sin la función range)
 """
-def ejercicio_1_for_en_vector ():
-    nombres = ['Lucas','Franco','Luciano','Malena','Jonatan','Luciano',
-               'Rodrigo','Maria Sol','Enzo','Milena']
-    for n in (nombres):
+
+
+def ejercicio_1_for_en_vector():
+    nombres = ['Lucas', 'Franco', 'Luciano', 'Malena', 'Jonatan', 'Luciano',
+               'Rodrigo', 'Maria Sol', 'Enzo', 'Milena']
+    for n in nombres:
         print(n)
+
 
 """
 Nombre de la función: ejercicio_2_for_en_vector En esta función agregar el 
@@ -18,19 +21,24 @@ vector: nombres = [‘Lucas’,’Franco’,’Luciano’,’Malena’,’Jonata
 Recorra el vector e imprima los elementos utilizando el ciclo for e indicando 
 en qué índice (posición del vector) se encuentra cada elemento.
 """
-#Alternativa 1 de hacerlo
-def ejercicio_2_for_en_vector_1 ():
-    nombres = ['Lucas','Franco','Luciano','Malena','Jonatan','Luciano',
-               'Rodrigo','Maria Sol','Enzo','Milena']
+
+
+# Alternativa 1 de hacerlo
+def ejercicio_2_for_en_vector_1():
+    nombres = ['Lucas', 'Franco', 'Luciano', 'Malena', 'Jonatan', 'Luciano',
+               'Rodrigo', 'Maria Sol', 'Enzo', 'Milena']
     nom = len(nombres)
-    for i in range (0,nom):
+    for i in range(0, nom):
         print(f'#{i} {nombres[i]}')
-#Alternativa 2 de hacerlo
-def ejercicio_2_for_en_vector_2 ():
+
+
+# Alternativa 2 de hacerlo
+def ejercicio_2_for_en_vector_2():
     nombres = ['Lucas', 'Franco', 'Luciano', 'Malena', 'Jonatan', 'Luciano',
                'Rodrigo', 'Maria Sol', 'Enzo', 'Milena']
     for index, l in enumerate(nombres):
         print(index, l)
+
 
 """
 Nombre de la función: ejercicio_3_for_en_vector En esta función agregar el 
@@ -43,13 +51,16 @@ restantes. Además mostrar el valor en la posición 1
 Resultado esperado: ’Jonatan’,’Luciano’,’Rodrigo’,’Maria Sol’,’Enzo’,’Milena’
 Elemento en posición 1: Franco
 """
-def ejercicio_3_for_en_vector ():
-    nombres = ['Lucas','Franco','Luciano','Malena','Jonatan','Luciano',
-               'Rodrigo','Maria Sol','Enzo','Milena']
+
+
+def ejercicio_3_for_en_vector():
+    nombres = ['Lucas', 'Franco', 'Luciano', 'Malena', 'Jonatan', 'Luciano',
+               'Rodrigo', 'Maria Sol', 'Enzo', 'Milena']
     nom = len(nombres)
-    for i in range(4,nom):
+    for i in range(4, nom):
         print(nombres[i])
     print(f'El elemento en posicion 1: {nombres[1]}')
+
 
 """
 Nombre de la función: ejercicio_4_while Realice una funcion que mientras la 
@@ -57,7 +68,9 @@ nota ingresada sea distinta de 0 le solicite al profesor el ingreso de otra
 nota, una vez que el profesor ingrese la nota 0 deberá imprimir un mensaje 
 con el promedio de todas las notas ingresadas.
 """
-def ejercicio_4_while ():
+
+
+def ejercicio_4_while():
     nota = 1
     cant_nota = 0
     nota_acum = 0
@@ -68,6 +81,8 @@ def ejercicio_4_while ():
             cant_nota += 1
     prom = nota_acum / cant_nota
     return prom
-    print(f'El promedio de las notas ingresadas es {ejercicio_4_while()}')
+
+
+print(f'El promedio de las notas ingresadas es {ejercicio_4_while()}')
 
 ejercicio_2_for_en_vector_2()
