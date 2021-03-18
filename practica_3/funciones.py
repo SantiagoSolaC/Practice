39c79ea82c161cb5c6f75c3b15b25d8d26068b2a
SantiagoSolaC <santiagosolacosta@gmail.com>
2021-03-18 15:45:36 -0300

    1

--- practica_3/funciones.py
+++ practica_3/funciones.py
@@ -1,4 +1,4 @@
-'''
+"""
 Una librería de la ciudad de Carlos Paz requiere de un programa que permita
 cargar los montos de todas las ventas realizadas en el mes. Para ello el
 programa debe permitir guardar ese valor en una lista.
@@ -13,11 +13,58 @@ Calcular el promedio de Ventas.
 Cada opción debe llamar a una función que se encargue de hacer el calculo
 correspondiente recibiendo la lista por parametro y retornando el valor
 correspondiente.
-'''
+"""
 
-lista = []
-def funcion_1 ():
+
+def monto_venta():
     monto = float(input('Monto abonado: '))
-    lista.append(monto)
+    return monto
 
-funcion_1()
+
+def mostrar_datos_lista(ventas):
+    lis = len(ventas)
+    for i in range(0, lis):
+        dato = ventas[i]
+        print(dato)
+
+
+def mostrar_venta_mas_baja(ventas):
+    x = ventas[0]
+    for i in ventas:
+        if i < x:
+            x = i
+    return x
+
+
+"""
+    Ó
+    minimo = min(lista)
+    print(minimo)
+    Ó
+    return min(lista)
+    """
+
+
+def suma_total(ventas):
+    ganancia = 0
+    largo_lista = len(ventas)
+    for i in range(0, largo_lista):
+        ganancia = ganancia + ventas[i]
+    return ganancia
+
+
+def promedio_ventas(ventas):
+    cant_venta = 0
+    suma_venta = 0
+    prom_ventas = 0
+    ven = len(ventas)
+    for i in range(0, ven):
+        suma_venta = suma_venta + ventas[i]
+        cant_venta = cant_venta + 1
+        prom_ventas = suma_venta / cant_venta
+    return prom_ventas
+
+
+def promedio_ventas_2(ventas):
+    prom_ventas = suma_total(ventas) / len(ventas)
+    return prom_ventas
---
+++ practica_3/principal.py
@@ -0,0 +1,39 @@
+from workspace.practica_3 import funciones
+
+
+def menu():
+    print('''---------------------------------------------------------------\n
+                        BIENVENIDO
+    #1 INGRESAR UNA VENTA
+    #2 MOSTRAR TODAS LAS VENTAS
+    #3 MOSTRAR UNICAMENTE LA VENTA MAS BAJA
+    #4 MOSTRAR LA GANANCIA TOTAL
+    #5 CALCULAR EL PROMEDIO DE LAS VENTAS
+    #0 PARA FINALIZAR\n
+---------------------------------------------------------------\n''')
+
+    opcion = input("Ingrese una opcion: ")
+    return opcion
+
+
+def menu_1():
+    ventas = []
+    opcion = menu()
+    while opcion != "0":
+        if opcion == "1":
+            a = funciones.monto_venta()
+            ventas.append(a)
+        elif opcion == "2":
+            funciones.mostrar_datos_lista(ventas)
+        elif opcion == "3":
+            print(f"La menor venta fue de: ${funciones.mostrar_venta_mas_baja(ventas)}")
+        elif opcion == "4":
+            print(f"La ganancia total es de: ${funciones.suma_total(ventas)}")
+        elif opcion == "5":
+            print(f"El promedio de las ventas es de: ${funciones.promedio_ventas_2(ventas)}")
+        else:
+            print("INGRESE UNA OPCION VALIDA!")
+        opcion = menu()
+
+
+menu_1()
